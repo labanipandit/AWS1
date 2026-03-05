42df0c69a7aa7910a30ab4aa428eb8e53fb45941
LABANI PANDIT <labanipandit141@gmail.com>
2026-03-05 10:53:48 +0530

    Update greeting message in index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', function (req, res) {
-  res.send('Hello MCKV LABANI HELLOOOO!!!!!!')
+  res.send('Hello MCKV LABANI HELLOOOO mine forever!!!!!!')
 })
 
 app.get('/courses', function (req, res) {
